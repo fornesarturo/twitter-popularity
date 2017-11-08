3073c76b8a280d98546443812397252330a6dfe9
fornesarturo <fornesarturo@gmail.com>
2017-11-07 18:48:10 -0600

    More RedisClient functionality, skellington for TwitterClient, and more requirements

--- RedisClient.py
+++ RedisClient.py
@@ -3,19 +3,37 @@ class RedisClient:
         import redis
         pool = redis.ConnectionPool(host=redisHost, port=redisPort, db=redisDb)
         self.redis = redis.Redis(connection_pool=pool)
+        self.toAdd = set()
+
+    def cleanRedis(self):
+        self.redis.flushdb()
 
     def populateRedis(self, pathToCSV):
-        import pandas as pd
+        if (self.redis.ping()):
+            import pandas as pd
+            csvNodes = pd.read_csv(pathToCSV)
+            for i, row in csvNodes.iterrows():
+                words = row['palabras'].split(" ")
+                value = row['valor']
 
-        csvNodes = pd.read_csv(pathToCSV)
-        for i, row in csvNodes.iterrows():
-            words = row['palabras'].split(" ")
-            value = row['valor']
+                for word in words:
+                    if self.redis.exists(word):
+                        self.redis.hincrby(word, "sum", amount=value)
+                        self.redis.hincrby(word, "count", amount=1)
+                    else:
+                        self.redis.hset(word, "count", amount=1)
+                        self.redis.hset(word, "sum", value)
 
-            for word in words:
-                if self.redis.exists(word):
-                    self.redis.hincrby(word, "sum", amount=value)
-                    self.redis.hincrby(word, "count", amount=1)
-                else:
-                    self.redis.hset(word, "count", amount=1)
-                    self.redis.hset(word, "sum", value)
+            return True
+        return False
+
+    def getPhraseValue(self, phrase):
+        phraseValue = 0
+        for word in phrase:
+            if self.redis.exists(word):
+                wordSum = int(self.redis.hget(word, "sum").decode())
+                wordCount = int(self.redis.hget(word, "count").decode())
+                phraseValue += wordSum / wordCount
+            else:
+                self.toAdd.add(phrase)
+        return phraseValue
--- TwitterClient.py
+++ TwitterClient.py
@@ -2,3 +2,22 @@ class TwitterClient:
     def __init__(self, AT, AT_S, CON, CON_S, redisHost='localhost', redisPort='6379', redisDb=0):
         import twitter
         self.twitter = twitter.api.Api(consumer_key=CON, consumer_secret=CON_S, access_token_key=AT, access_token_secret=AT_S, tweet_mode='extended')
+
+    def getTweets(self, queryParam):
+        # Get Tweets and put them somewhere locally in this class or a
+        # temp_file.
+        return
+
+    def cleanData(self, data):
+        # Clean the data you got from getTweets, again, store in this class or
+        # in a temp_file.
+        return
+
+    def toCSV(self):
+        # Maybe call this function after getTweets or cleanData to store the
+        # output as a CSV.
+        return
+
+    def fromCSV(self, pathToCSV):
+        # To read from a CSV into methods like cleanData
+        return
