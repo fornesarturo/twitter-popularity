8edb967b21a617b08b2712aeb94f02201bc445b7
Miguel Montoya <miguel.mzbi@gmail.com>
2017-11-07 18:53:53 -0600

    StopWords

--- tweet.py
+++ tweet.py
@@ -1,14 +1,26 @@
 from twitter import *
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 def getLastEPNTweet(AT,AT_S,CON,CON_S):
     
     t = Twitter(auth=OAuth(AT,AT_S,CON,CON_S))
 
-    tweets = list(t.statuses.user_timeline(screen_name="EPN",count=1))
+    print("Busqueda sobre EPN")
+    tweets = t.search.tweets(q="@EPN")
+    for tweet in tweets['statuses']:
+        if not(tweet['text'][0] == 'R' and tweet['text'][1] == 'T'):
+            print(tweet['text'])
 
-    #for tweet in tweets:
-        #print(tweet['text'])
-    return tweets[0]['text'] + " - EPN" 
+    print()
+    print("Tweets de EPN")
+    tweets2 = t.statuses.user_timeline(screen_name="EPN")
+    for tweet in tweets2:
+        if not(tweet['text'][0] == 'R' and tweet['text'][1] == 'T'):
+            print(tweet['text'])
+    
+    #pp.pprint(tweets2)
+    
 
 AT = "880668541-hAfZ3IqqTgc1aJNjJzEojfjX0tD9rHF9Q6zNQ3BZ"
 AT_S = "tl98z1tdnCPRWx8YY4HkZVxbbEoaXfJv4XTmNazFvBb8y"
@@ -16,3 +28,5 @@ CON = "kgFzF4dC6jJC1bynLhOSvWFrV"
 CON_S = "gKX4yAWrSamlGrh9iatOjcfUsrenjqyl4QnfmvvDQMocds0XHv" 
 
 print(getLastEPNTweet(AT,AT_S,CON,CON_S))
+
+
