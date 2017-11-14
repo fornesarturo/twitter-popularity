7a894a2f0d4efc69ca2cd3701f1acdb48c74f77d
fornesarturo <fornesarturo@gmail.com>
2017-11-13 18:22:13 -0600

    Now with less data=None

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ def main():
     twitterClient = setClient()
     redisClient = RedisClient()
 
-    username = "lopezobrador_" #input("¿Qué usuario quieres investigar? ")
+    username = "g_quadri"  # input("¿Qué usuario quieres investigar? ")
     date = "2017-11-13" #input("Fecha en formato yyyy-mm-dd: ")
+    
     twitterClient.getTweets(username, dt.datetime.now().strftime("%Y-%m-%d"))
     cleanedData = twitterClient.cleanData(path="rawtweets.csv")
-    twitterClient.prettyP(cleanedData)
+    # twitterClient.prettyP(cleanedData)
+    
     twitterClient.saveTweetsCSV([tweet[0] for tweet in cleanedData.values() if len(tweet[0]) > 0])
-    popularity = redisClient.calculatePopularity(pathToCSV="rawtweets.csv", data=None)
+    popularity = redisClient.calculatePopularity(pathToCSV="rawtweets.csv")
+    
     print("{user} - {date}: {popularity}".format(user=username, date=date, popularity=popularity))
 
 if __name__ == '__main__':
